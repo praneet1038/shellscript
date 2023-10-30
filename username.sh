63d62ed8b55ea909c921df37db3a21db4284c3d2
praneeth <praneeth1038@gmail.com>
2023-10-30 17:42:26 +0100

    added username script

---
+++ username.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+echo "This script is executed on $(date)"
+echo "please enter username "
+echo "**********************"
+read x
+echo "The entered username is $x"
+echo "Please enter password"
+read -s y
+echo "enetered password is $y"
